fba5337b4bd84c89ea3672340490cd289e506a64
Александр Румянцев <pitchie@list.ru>
2022-10-31 16:54:11 +0300

    migrations

---
+++ Wheels.Persistence/Migrations/20221031071935_Del.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wheels.Persistence.Migrations
+{
+    public partial class Del : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                            name: "WasDeleted",
+                            table: "Posts",
+                            type: "boolean",
+                            nullable: false,
+                            defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+            migrationBuilder.DropColumn(
+                                                     name: "WasDeleted",
+                                                     table: "Posts");
+        }
+    }
+}
